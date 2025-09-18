c7b6e4cdb74e8cd541f841c64eb038740d5fae40
DiprodipDas <diprodip755@gmail.com>
2025-09-18 11:41:49 +0600

    second commit

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Home from './pages/home/Home.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+    <Route element={<App/>}>
+     <Route path='/' element={<Home></Home>}/>
+     <Route path='/pricing' element={<div>Pricing Page</div>}/>
+     <Route path='/services' element={<div>services Page</div>}/>
+     <Route path='/blog' element={<div>Blog Page</div>}/>
+     <Route path='/about' element={<div>About page</div>}/>
+     
+    </Route>
+    </Routes>
+  </BrowserRouter>,
+)
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+import ToolSection from './ToolSection'
+import CompanyLogos from './CompanyLogos'
+import Services from './Services'
+
+const Home = () => {
+  return (
+    <>
+    <HeroSection></HeroSection>
+    <ToolSection></ToolSection>
+    <CompanyLogos></CompanyLogos>
+    <Services></Services>
+    </>
+  )
+}
+
+export default Home
