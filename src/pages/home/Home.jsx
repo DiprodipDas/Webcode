6688975dd7aadd7edfa3e9037763b50a768c63dc
DiprodipDas <diprodip755@gmail.com>
2025-09-18 17:28:40 +0600

    blog section done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import ToolSection from './ToolSection'
 import CompanyLogos from './CompanyLogos'
 import Services from './Services'
 import Testimonials from './Testimonials'
+import Blogs from './Blogs'
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
     <CompanyLogos></CompanyLogos>
     <Services></Services>
     <Testimonials></Testimonials>
+    <Blogs></Blogs>
     </>
   )
 }
