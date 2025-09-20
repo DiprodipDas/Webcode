8aaf4aec50d7aa7ebbd09ce3cbb65dd184c62c36
DiprodipDas <diprodip755@gmail.com>
2025-09-20 13:56:43 +0600

    register and login page design complete

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,29 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from './pages/home/Home.jsx'
+import ErrorPage from './components/ErrorPage.jsx'
+import Pricing from './pages/home/Pricing.jsx'
+import Services from './pages/home/Services.jsx'
+import Blogs from './pages/home/Blogs.jsx'
+import Register from './components/Register.jsx'
+import Login from './components/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
     <Route element={<App/>}>
      <Route path='/' element={<Home></Home>}/>
-     <Route path='/pricing' element={<div>Pricing Page</div>}/>
-     <Route path='/services' element={<div>services Page</div>}/>
-     <Route path='/blog' element={<div>Blog Page</div>}/>
+     <Route path='/pricing' element={<div>{<Pricing/>}</div>}/>
+     <Route path='/services' element={<div>{<Services/>}</div>}/>
+     <Route path='/blog' element={<div>{<Blogs/>}</div>}/>
      <Route path='/about' element={<div>About page</div>}/>
+
+
+     <Route path='*' element={<div><ErrorPage/></div>}/>
      
     </Route>
+    <Route path='/register' element={<Register/>}/>
+    <Route path='/login' element={<Login/>}/>
     </Routes>
   </BrowserRouter>,
 )
